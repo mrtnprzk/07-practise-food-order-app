724d72eb3d5e12d38d6b98409d43e4c1775e8779
Martin Parizek <marti.parizek@gmail.com>
2022-06-18 16:38:52 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-function App() {
+import React, { useState } from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
+
+const App = () => {
+
+  const [cartShown, setCartShown] = useState(false);
+
+  const showCartHandler = () => {setCartShown(true);};
+  const hideCartHandler = () => {setCartShown(false);};
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <CartProvider>
+      {cartShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
-}
+};
 
 export default App;
